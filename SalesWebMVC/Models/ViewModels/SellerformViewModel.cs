84572de8a077b308d815107424d7b4d8ff44056f
LucasMartinelli <lucasbmartinelli@gmail.com>
2021-01-25 16:27:24 -0300

    first commit

---
+++ SalesWebMVC/Models/ViewModels/SellerformViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models.ViewModels
+{
+    public class SellerformViewModel
+    {
+        public Seller Seller { get; set; }
+        public ICollection<Department> Departments { get; set; }
+    }
+}
---
+++ SalesWebMVC/Startup.cs
@@ -0,0 +1,82 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Pomelo.EntityFrameworkCore.MySql;
+using SalesWebMVC.Data;
+using SalesWebMVC.Models;
+using SalesWebMVC.Services;
+using System.Collections.Generic;
+
+namespace SalesWebMVC
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            string mySqlConnection = Configuration.GetConnectionString("SalesWebMVCContext");
+
+            services.AddDbContextPool<SalesWebMVCContext>(options =>
+                    options.UseMySql(mySqlConnection,
+                    ServerVersion.AutoDetect(mySqlConnection), builder =>
+                    builder.MigrationsAssembly("SalesWebMVC")));
+
+            services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
+            services.AddScoped<DepartmentService>();
+            services.AddScoped<SalesRecordService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SeedingService seedingService)
+        {
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+            app.UseRequestLocalization(localizationOptions);
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                seedingService.Seed();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
